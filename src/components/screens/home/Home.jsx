d7f4c63ed5d9176610c748c32675bd913ac41488
Vincent_tt <evgeniy161102@gmail.com>
2023-07-17 00:42:43 +0300

    __

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,16 +1,26 @@
+import styles from "./Home.module.scss";
 import { useNavigate } from "react-router-dom";
 import { Layout } from "../../Layout/Layout";
-
+import { Button } from "../../UI/Button/Button";
+import { useAuth } from "../../../hooks/useAuth";
 function Home() {
   /* 
     todo
 
-    - Продолжить разработку по 7 16:15 уроку "Frontend для начинающих"
+    - Продолжить разработку по 8 уроку "Frontend для начинающих"
   */
-
+  const { isAuth } = useAuth();
   const navigate = useNavigate();
 
-  return <Layout bgImage="/images/home-bg.jpg"></Layout>;
+  return (
+    <Layout bgImage="/images/home-bg.jpg">
+      <Button clickHandler={() => navigate(isAuth ? "/new-workout" : "/auth")}>
+        {isAuth ? "New" : "Sign in"}
+      </Button>
+      <h1 className={styles.heading}>EXERCISES FOR THE SHOULDERS</h1>
+      {/* TODO: Counters */}
+    </Layout>
+  );
 }
 
 export default Home;
